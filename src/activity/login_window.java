767729494c529e512e7913939e716bcb2fe0fb4e
Mahee Gamage <154115K@uom.lk>
2018-06-08 20:34:22 +0530

    Done Login

--- src/activity/login_window.java
+++ src/activity/login_window.java
@@ -1,12 +1,22 @@
 package activity;
 
 import java.awt.EventQueue;
-
+import java.sql.*;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JSeparator;
 
 public class login_window {
 
-	private JFrame frame;
+	private JFrame frame, frmLoginSystem;
+	private JTextField txtUsername;
+	private JPasswordField txtPassword;
 
 	/**
 	 * Launch the application.
@@ -36,8 +46,101 @@ public class login_window {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(200, 200, 500, 339);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Login System");
+		lblNewLabel.setBounds(195, 13, 97, 16);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblUsername = new JLabel("Username");
+		lblUsername.setBounds(58, 76, 73, 16);
+		frame.getContentPane().add(lblUsername);
+		
+		JLabel lblPassword = new JLabel("Password");
+		lblPassword.setBounds(58, 147, 56, 16);
+		frame.getContentPane().add(lblPassword);
+		
+		txtUsername = new JTextField();
+		txtUsername.setBounds(239, 73, 199, 22);
+		frame.getContentPane().add(txtUsername);
+		txtUsername.setColumns(10);
+		
+		txtPassword = new JPasswordField();
+		txtPassword.setBounds(239, 144, 199, 22);
+		frame.getContentPane().add(txtPassword);
+		
+		JButton btnLogin = new JButton("Login");
+		btnLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				try {
+					Class.forName("com.mysql.jdbc.Driver");
+					Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/criminal_records","root","");
+					Statement stmt = con.createStatement();
+					String sql = "SELECT * FROM loginUser WHERE username ='"+ txtUsername.getText() + "' AND password = '"+ txtPassword.getText().toString() +"'";
+					ResultSet rs= stmt.executeQuery(sql);
+					if(rs.next()) {
+						JOptionPane.showMessageDialog(null, "Login Successfully....");
+					}
+					else {
+						JOptionPane.showMessageDialog(null, "Login Failed");
+					}
+					con.close();
+						
+				} 
+				catch(Exception e) 
+				{
+					System.out.print(e);
+				}
+				
+//				String password = txtPassword.getText();
+//				String username = txtUsername.getText();
+//				
+//				if(password.contains("king") && username.contains("one")) {
+//					txtPassword.setText(null);
+//					txtUsername.setText(null);
+//				}
+//				else {
+//					JOptionPane.showMessageDialog(null, "Invalid Login Details", "Login Error",JOptionPane.ERROR_MESSAGE);
+//					txtPassword.setText(null);
+//					txtUsername.setText(null);
+//				}
+			}
+		});
+		btnLogin.setBounds(34, 215, 97, 25);
+		frame.getContentPane().add(btnLogin);
+		
+		JButton btnReset = new JButton("Reset");
+		btnReset.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				txtUsername.setText(null);
+				txtPassword.setText(null);
+			}
+		});
+		btnReset.setBounds(195, 215, 97, 25);
+		frame.getContentPane().add(btnReset);
+		
+		JButton btnExit = new JButton("Exit");
+		btnExit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frmLoginSystem = new JFrame("Exit");
+				if(JOptionPane.showConfirmDialog(frmLoginSystem, "Cofirm if you want to exit", "Login System",
+						JOptionPane.YES_NO_OPTION) == JOptionPane.YES_NO_OPTION) {
+					System.exit(0);
+				}
+			}
+		});
+		btnExit.setBounds(352, 215, 97, 25);
+		frame.getContentPane().add(btnExit);
+		
+		JSeparator separator = new JSeparator();
+		separator.setBounds(12, 198, 458, 2);
+		frame.getContentPane().add(separator);
+		
+		JSeparator separator_1 = new JSeparator();
+		separator_1.setBounds(12, 50, 458, 2);
+		frame.getContentPane().add(separator_1);
 	}
-
 }
